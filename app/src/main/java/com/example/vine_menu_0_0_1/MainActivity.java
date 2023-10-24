e882dd6b4863f341d6eaa60f905f84f1baf616bf
Nikita Sachik <nikitasachik@gmail.com>
2023-10-25 01:10:06 +0300

    Empty project

---
+++ app/src/main/java/com/example/vine_menu_0_0_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vine_menu_0_0_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Vine_menu_0_0_1"
+include(":app")
+ 
