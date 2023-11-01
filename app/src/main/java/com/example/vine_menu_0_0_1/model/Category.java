3f3e3d5b689bdf230707b5bbba02cc6dc22309a1
Nikita Sachik <nikitasachik@gmail.com>
2023-11-01 12:45:28 +0200

    added categories

---
+++ app/src/main/java/com/example/vine_menu_0_0_1/model/Category.java
@@ -0,0 +1,27 @@
+package com.example.vine_menu_0_0_1.model;
+
+public class Category {
+    int id;
+    String title;
+
+    public Category(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
